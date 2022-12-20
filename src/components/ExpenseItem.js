e2145c3b02362f4172101d060c4712760c4150ba
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-20 11:29:30 +0200

    feat: add logic for displaying the day and the year in ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,15 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   const month = props.date.toLocaleDateString("en-US", { month: "long" });
+  const day = props.date.toLocaleDateString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
       <div>
         <div>{month}</div>
-        <div>Year</div>
-        <div>Day</div>
+        <div>{year}</div>
+        <div>{day}</div>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
