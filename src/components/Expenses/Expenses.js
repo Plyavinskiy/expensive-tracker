999f5a1a10155b4c70eb65db30628767b48b919c
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-12-23 03:19:46 +0200

    feat: pass the selected year from ExpensesFilter to Expenses (one level up)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,10 +4,15 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const filterChangeHandler = (selectedYear) => {
+    console.log("Expenses.js");
+    console.log(selectedYear);
+  };
+
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter />
+        <ExpensesFilter onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
